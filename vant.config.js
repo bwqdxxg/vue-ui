6ea09b9a70580aa1b8473e6985a2659029310c5b
董俊杰 <215943108@qq.com>
2021-07-21 18:02:27 +0800

    1.修改说明

--- vant.config.js
+++ vant.config.js
@@ -19,7 +19,7 @@ module.exports = {
     // 图标
     logo: 'https://www.npmjs.com/npm-avatar/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXJVUkwiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci8zZTVkNWRjNzY2YzczYjgxM2U4YTkxZDViMzI5MmFlMT9zaXplPTUwJmRlZmF1bHQ9cmV0cm8ifQ.ZX3UKI2x1KC7Zd0P6EtwLexV9Kbq9yQP7AzKGzEDOZI', // 'https://img.yzcdn.cn/vant/logo.png',
     // 描述
-    description: '轻量、可靠的移动端 Vue 组件库',
+    description: '轻量、可靠的 Vue 组件库',
     links: [
       {
         logo: 'https://b.yzcdn.cn/vant/logo/github.svg',
